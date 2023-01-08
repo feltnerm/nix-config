fcf192ad672a0dd306bf9aefc3ca392f4378f08c
feltnerm <feltner.mj@gmail.com>
2023-01-08 09:47:53 -0600

    feat(hosts/markbook): flesh out first darwin host

--- hosts/markbook/default.nix
+++ hosts/markbook/default.nix
@@ -14,6 +14,33 @@
   config = {
     feltnerm = {};
 
+    homebrew.casks = [
+      # tiling window manager
+      "amethyst"
+
+      # Visual size
+      "disk-inventory-x"
+
+      # Dreams
+      "electric-sheep"
+
+      # For Java
+      "intellij-idea-ce"
+
+      # block stuff
+      "minecraft"
+
+      # VPN
+      "private-internet-access"
+
+      # it's meh, but good visual editor
+      "visual-studio-code"
+
+      # VM mgmt
+      "vagrant"
+      "virtualbox"
+    ];
+
     # system.copySystemConfiguration = false;
     # system.stateVersion = 22;
   };
