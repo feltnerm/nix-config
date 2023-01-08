d4e6af4294db88b2934884281903cf420357ffa6
feltnerm <feltner.mj@gmail.com>
2023-01-08 12:32:05 -0600

    chore: add darwin modules

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@
 {
   modules = import ./modules/nixos;
   homeManagerModules = import ./modules/home-manager;
+  darwinModules = import ./modules/darwin;
 }
 # Import packages to top-level
 // (import ./pkgs {inherit pkgs;})
