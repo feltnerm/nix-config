598d0f30e1ae364dfa524e3c2bf199a9700b37b1
feltnerm <mark.feltner@acquia.com>
2024-03-22 16:08:56 -0500

    feat(packages): add nix-format-feltnerm
    this is mostly how i format my nix files

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,7 @@
 {pkgs}: {
   chuckscii = pkgs.callPackage ./chuckscii.nix {};
   greet = pkgs.callPackage ./greet.nix {};
+  nix-format-feltnerm = pkgs.callPackage ./nix-format-feltnerm.nix {};
   nlsp = pkgs.callPackage ./nlsp.nix {};
   screensaver = pkgs.callPackage ./screensaver.nix {};
   year-progress = pkgs.callPackage ./year-progress.nix {};
