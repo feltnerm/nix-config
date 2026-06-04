bbe0df19d95cbbeb1d788e69c9d05d1b1d3f1235
feltnerm <feltner.mj@gmail.com>
2026-06-03 19:58:43 -0500

    fix(codemonkey): new kbd

--- configs/nixos/codemonkey/hardware.nix
+++ configs/nixos/codemonkey/hardware.nix
@@ -172,7 +172,7 @@
       enable = true;
       keyboards.internalKeyboard = {
         devices = [
-          "/dev/input/by-id/usb-Keychron_Keychron_K2_Pro-event-kbd"
+          "/dev/input/by-id/usb-Keychron_Keychron_K3-event-kbd"
         ];
         extraDefCfg = "process-unmapped-keys yes";
         # keep full keyboard config in main config to avoid duplication
